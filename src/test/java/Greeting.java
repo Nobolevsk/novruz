0310a01f4ebd19d0dee416a4cbc1ac661259629f
Novruz <nobolevsk@gmail.com>
2017-12-03 19:34:36 +0300

    Greeting

---
+++ src/test/java/Greeting.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Новруз on 03.12.2017.
+ */
+public class Greeting {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+	}
+}
--- src/test/java/name.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Новруз on 01.12.2017.
- */
-public class name {
-	public static void main(String[] args) {
-		
-	}
-}
