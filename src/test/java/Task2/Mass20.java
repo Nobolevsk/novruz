720092f652822393cb2844e56a2fc1782b670b98
Novruz <nobolevsk@gmail.com>
2017-12-23 23:37:15 +0300

    Refresh Mass20

--- src/test/java/Task2/Mass20.java
+++ src/test/java/Task2/Mass20.java
@@ -1,5 +1,7 @@
 package Task2;
 
+import java.util.Arrays;
+
 /**
  * Created by Новруз on 16.12.2017.
  */
@@ -7,23 +9,32 @@ public class Mass20 {
 	public static void main(String[] args) {
 		int[] mass = new int[20];
 
-		int max = 0;
-		int min = 0;
+		int max = 10;
+		int maxI = 0;
+		int min = -10;
+		int minI = 0;
 		for (int i = 0; i < mass.length; i++) {
 			mass[i] = -10 + (int) (Math.random() * (11 - (-10)));
-			if (mass[i] > max)
+			System.out.print(Arrays.asList(mass[i]));
+			if (mass[i] <= max && mass[i] > 0) {
 				max = mass[i];
-			if (mass[i] < min)
+				maxI = i;
+			}
+			int a = mass[i];
+			if (mass[i] >= min && mass[i] < 0) {
 				min = mass[i];
+				minI = i;
+			}
 		}
-		System.out.println("Минимально положительный элемент массива: " + max +
+
+		System.out.println("\nМинимально положительный элемент массива: " + max +
 				"\nМаксимально минимальный элемент массива: " + min);
 
-		int maxmin = max;
-		max = min;
-		min = max;
-		System.out.println("\n↓↓↓После обмена значениями получаем↓↓↓\n" +
-				"Минимально положительный элемент массива: " + max +
-				"\nМаксимально минимальный элемент массива: " + min);
+		System.out.println("↓↓↓После обмена значениями получаем↓↓↓");
+		mass[maxI] = min;
+		mass[minI] = max;
+		for (int i = 0; i < mass.length; i++) {
+			System.out.print(Arrays.asList(mass[i]));
+		}
 	}
 }
