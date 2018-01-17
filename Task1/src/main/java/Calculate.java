5c8d1020cc9b97316a590fe8c95a19f44e5e0ce6
Novruz <nobolevsk@gmail.com>
2018-01-18 01:11:15 +0300

    start

---
+++ Task1/src/main/java/Calculate.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+/**
+ * Created by Новруз on 03.12.2017.
+ */
+public class Calculate {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Print the first number ( only double type)");
+		double a = scanner.nextDouble();
+		System.out.println("Print the second number (only double type)");
+		double b = scanner.nextDouble();
+		System.out.println("Print the third number (only double type)");
+		double c = scanner.nextDouble();
+
+		System.out.println("What the operation do you want to do (+,-,*,/)?");
+		String d = scanner.next();
+
+		double e = a + b + c;
+		if (d.equals("+"))
+			System.out.printf("Your sum result is: %.4f", e);
+
+		double f = a - b - c;
+		if (d.equals("-"))
+			System.out.printf("Your minus result is: %.4f", f);
+
+		double g = a * b * c;
+		if (d.equals("*"))
+			System.out.printf("Your multipl. result is: %.4f", g);
+
+		double k = a / b / c;
+		if (d.equals("/"))
+			System.out.printf("Your div result is: %.4f", k);
+
+
+		scanner.close();
+	}
+}
---
+++ Task1/src/main/java/Greeting.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Новруз on 01.12.2017.
+ */
+public class Greeting {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+	}
+}
