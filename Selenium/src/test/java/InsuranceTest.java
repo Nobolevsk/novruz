9d7b9ca4129efce150090b46b09e8d5a2b0eb825
Novruz <nobolevsk@gmail.com>
2018-01-18 22:46:13 +0300

    start

--- Selenium/src/test/java/InsuranceTest.java
+++ Selenium/src/test/java/InsuranceTest.java
@@ -1,5 +1,12 @@
 /**
  * Created by Новруз on 18.01.2018.
  */
+
+
 public class InsuranceTest {
+	public static void main(String[] args) {
+		//System.setProperty("webdriver.chrome.driver", "drv/chromedriver.exe");
+		System.out.println("ll");
+	}
+
 }
